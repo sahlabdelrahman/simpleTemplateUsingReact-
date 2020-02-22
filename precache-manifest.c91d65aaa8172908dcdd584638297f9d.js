35ce3a5d42b11ef3ccea5a1f91caf4f22210ba56
sahl abdelrahman <bnabdelrahman07@gmail.com>
2020-02-22 16:13:39 +0300

    Updates

---
+++ precache-manifest.c91d65aaa8172908dcdd584638297f9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15020af1243d58bd191c966c37941549",
+    "url": "/simpleTemplateUsingReact/index.html"
+  },
+  {
+    "revision": "25f517633a8344352a96",
+    "url": "/simpleTemplateUsingReact/static/css/main.9299351d.chunk.css"
+  },
+  {
+    "revision": "6fd8d379ada58b60d116",
+    "url": "/simpleTemplateUsingReact/static/js/2.03aa0e51.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/simpleTemplateUsingReact/static/js/2.03aa0e51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f517633a8344352a96",
+    "url": "/simpleTemplateUsingReact/static/js/main.c47cdb1d.chunk.js"
+  },
+  {
+    "revision": "ead88577fdaab060270a",
+    "url": "/simpleTemplateUsingReact/static/js/runtime-main.576111ba.js"
+  }
+]);
